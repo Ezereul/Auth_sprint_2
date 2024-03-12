102571c9f701d1cf41f4234d691692ac71984fad
Maxim Isaev <isaev_0602@mail.ru>
2024-03-12 19:55:34 +0500

    another redis db for rate limit

--- auth/src/api/middlewares.py
+++ auth/src/api/middlewares.py
@@ -2,12 +2,12 @@ from typing import Callable
 
 from fastapi import Request, Response
 
-from auth.src.db.redis import get_redis
+from auth.src.db.redis import get_redis_rate_limit
 from auth.src.utils.rate_limit import is_request_allowed, extract_user_id
 
 
 async def rate_limit_middleware(request: Request, call_next: Callable):
-    redis_instance = await get_redis()
+    redis_instance = await get_redis_rate_limit()
     user_id = await extract_user_id(request)
 
     is_allowed = await is_request_allowed(user_id, redis_instance)
