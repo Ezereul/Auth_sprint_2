1bbdc3821ea790d92c48a8425d9196f3f288be70
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-03-13 03:27:26 +0300

    basic `.env` variables renaming WIP, just stash commit

--- movies_api/main.py
+++ movies_api/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
     description='Information regarding films, genres and people who took part in the films creation',
-    version='1.0.0'
+    version=settings.version
 )
 
 
