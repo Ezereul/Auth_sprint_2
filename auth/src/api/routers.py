178e99702e26d2ce0f68c38695b67e119f002fd4
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-03-15 17:27:02 +0300

    Auth routers fix

--- auth/src/api/routers.py
+++ auth/src/api/routers.py
@@ -2,17 +2,17 @@ from fastapi import APIRouter
 
 from auth.src.api.v1 import account_router, auth_router, history_router, role_router
 
-main_router = APIRouter()
+main_router = APIRouter(prefix='/api/v1')
 
 main_router.include_router(
-    auth_router, prefix='/api/v1/auth', tags=['User Authentication']
+    auth_router, prefix='/auth', tags=['User Authentication']
 )
 main_router.include_router(
-    history_router, prefix='/api/v1/history', tags=['Login History']
+    history_router, prefix='/history', tags=['Login History']
 )
 main_router.include_router(
-    role_router, prefix='/api/v1/role', tags=['Roles']
+    role_router, prefix='/role', tags=['Roles']
 )
 main_router.include_router(
-    account_router, prefix='/api/v1/account', tags=['Account']
+    account_router, prefix='/account', tags=['Account']
 )
