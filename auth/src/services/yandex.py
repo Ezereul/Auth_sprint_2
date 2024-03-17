e0edf07fa589a1a1d46a21ff3a4a8fe20e2bddc9
Maxim Isaev <isaev_0602@mail.ru>
2024-03-17 19:58:41 +0500

    метод для создания SocialAccount

--- auth/src/services/yandex.py
+++ auth/src/services/yandex.py
@@ -26,27 +26,32 @@ class YandexService:
             )
         ))).first()
 
-    async def new(self, session: AsyncSession, social_id: str, login: str):
-        if (social_acc := await self.get(session, social_id)) is not None:
-            return await self.user_service.get(session, social_acc.user_id), False, None
-
-        if (user := await self.user_service.get_by_name(session, login)) is not None:
-            return user, False, None
-
-        random_password = generate_random_string()
-        default_role = await self.role_service.get_default_role(session)
-        user = await self.user_service.create(
-            session=session, username=login, password=random_password, role=default_role
-        )
-
+    async def create(self, session: AsyncSession, user_id: str, social_id: str):
         social_acc = SocialAccount(
-            user_id=user.id,
+            user_id=user_id,
             social_id=social_id,
             social_name=self.social_name
         )
 
         session.add(social_acc)
         await session.commit()
+
+    async def new(self, session: AsyncSession, social_id: str, login: str):
+        if (social_acc := await self.get(session, social_id)) is not None:
+            return await self.user_service.get(session, social_acc.user), False, None
+
+        if (user := await self.user_service.get_by_name(session, login)) is not None:
+            await self.create(session, user.id, social_id)
+            return user, False, None
+
+        random_password = generate_random_string()
+        default_role = await self.role_service.get_default_role(session)
+
+        user = await self.user_service.create(
+            session=session, username=login, password=random_password, role=default_role
+        )
+
+        await self.create(session, user.id, social_id)
 
         return user, True, random_password
 
