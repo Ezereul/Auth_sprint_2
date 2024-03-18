3b6e1093e346aabbb4eb845cb826d65d672fccbe
Maxim Isaev <isaev_0602@mail.ru>
2024-03-18 13:45:48 +0500

    Порт и хост для Jaeger вынесены в .env

--- auth/src/api/tracers.py
+++ auth/src/api/tracers.py
@@ -3,14 +3,16 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
+from auth.src.core.config import settings
+
 
 def setup_tracing() -> None:
     trace.set_tracer_provider(TracerProvider())
     trace.get_tracer_provider().add_span_processor(
         BatchSpanProcessor(
             JaegerExporter(
-                agent_host_name='localhost',
-                agent_port=6831,
+                agent_host_name=settings.jaeger.agent_host_name,
+                agent_port=settings.jaeger.agent_port,
             )
         )
     )
